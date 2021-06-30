7b93d3e22ab43b4593655c292d0400e99f88a248
unknown <salkin.santiago@gmail.com>
2021-06-30 18:17:08 -0300

    cart quantity notification in CartWidget

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -2,10 +2,22 @@ import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 
+import useCartContext from "../context/CartContext";
 
 const CartWidget = () => {
-    return (        
-        <FontAwesomeIcon icon={faShoppingCart} />
+
+    const {itemsInCart} = useCartContext();
+
+    return (   
+        <div>
+            <FontAwesomeIcon icon={faShoppingCart} />
+            {itemsInCart > 0?
+                <div class="w-10 ml-2 h-8 inline-flex items-center justify-center rounded-full bg-red-500 text-red-100 mb-1">
+                    <p>{itemsInCart}</p>
+                </div>       
+            : <span className="hidden">Cart Placeholder</span>
+            }
+        </div>
     );
 }
 
