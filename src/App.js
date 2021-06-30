b967de087a2c59a2253cb8e01376daa4afcc2daf
unknown <salkin.santiago@gmail.com>
2021-06-30 18:01:26 -0300

    added CartContext - fixes in ItemDetail

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 
+import { CartProvider } from "./context/CartContext";
+
+
 import AppRouter from './routing/AppRouter';
 
 function App() {  
-  return (    
-     <AppRouter></AppRouter>
+  return (       
+    <CartProvider>     
+      <AppRouter />
+    </CartProvider>
   );
 }
 
