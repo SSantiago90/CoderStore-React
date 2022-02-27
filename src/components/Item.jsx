3752e2470fc8a203f95c5b5f44495df6a2342c5b
SSantiago90 <salkin.santiago@gmail.com>
2022-02-27 15:27:19 -0300

    itemlist imgloader

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -2,12 +2,23 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
+import OnImagesLoaded from "react-on-images-loaded";
+import ImageLoader from "./loaders/ImgLoader";
+
 const Item = (props) => {
+    const [imgLoaded, setImgLoaded] = React.useState(false);
+
     return (
         <div className="xl:w-1/3 md:w-1/2 p-4">
             <div className="shadow-lg bg-gray-100 p-6 rounded-lg">
             <Link to={`/products/${props.id}`}>
-                <img className="object-contain bg-placeholder h-60 rounded w-full object-center mb-6" src={props.imgUrl} alt={props.title}/>
+                <OnImagesLoaded onLoaded={()=> setImgLoaded(true)} >        
+                <div className={imgLoaded ? 'hidden-false' : 'hidden-true'}>
+                    <img alt={props.title} className="image object-contain bg-placeholder h-60 rounded w-full object-center mb-6" src={props.imgUrl}/>
+                </div>
+                {  !imgLoaded && <ImageLoader size={64} /> }      
+                </OnImagesLoaded>
+                
             </Link>
                 <h3 className="h-20 tracking-widest text-grey-800 font-bold title-font">{props.title}</h3>
                 <span className="title-font font-medium text-2xl text-gray-900">
--- src/components/loaders/ImgLoader.jsx
+++ src/components/loaders/ImgLoader.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const ImgLoader = () => {
+const ImgLoader = ({size = 96}) => {
+    const sizeCSS = `m-auto rounded-full bg-gray-300 h-${size} w-${size}`;
   return (
-        <div className="mx-auto animate-pulse flex space-x-12">
-                <div className="m-auto rounded-full bg-gray-300 h-96 w-96"></div>           
+        <div className="mx-auto mb-10 animate-pulse flex space-x-12">
+                <div className={sizeCSS}></div>           
             </div>
   );
 };
