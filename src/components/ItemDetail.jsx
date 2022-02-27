2bb6a173839a8d3b9b8b34aa12a9363adc4616ce
SSantiago90 <salkin.santiago@gmail.com>
2022-02-27 15:17:31 -0300

    detail loader & image loader

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,32 +1,40 @@
-import React from 'react';
+import React from "react";
 import ItemCount from "./ItemCount";
 import { Link, useHistory } from "react-router-dom";
 import useCartContext from "../context/CartContext";
 
-import { faArrowCircleLeft, faArrowCircleRight } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import ImageLoader from "./loaders/ImgLoader";
+import './styles/loaders.css';
+
+import OnImagesLoaded from "react-on-images-loaded";
+
+import {  faArrowCircleLeft,  faArrowCircleRight } from "@fortawesome/free-solid-svg-icons";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function ItemDetail(props) {
-  const {addItem, isInCart, qtyInCart} = useCartContext();
+  const { addItem, isInCart, qtyInCart } = useCartContext();
+  const [imgLoaded, setImgLoaded] = React.useState(false);
+  const navig = useHistory();
 
-  const navig = useHistory(); 
+  function onAdd(countValue) {
+    isInCart(props.id)
+      ? alert("Ya agregaste ese item al carrito!")
+      : addItem(props, countValue);
+  }
 
-  function onAdd(countValue){
-    isInCart(props.id)?
-      alert('Ya agregaste ese item al carrito!')
-      : addItem( props, countValue);    
-  }  
-  
+
   return (
     <section className="relative text-gray-600 body-font overflow-hidden">
       <div className="container px-5 py-24 mx-auto">
         <div className="lg:w-4/5 mx-auto flex flex-wrap">
-          <div className="img-loader lg:w-1/2 w-full lg:h-auto h-32 object-scale-down object-center rounded">
-            <img
-              alt={props.title}
-              className=""
-              src={props.imgUrl}
-            />
+          <div className="lg:w-1/2 w-full lg:h-auto h-32 object-scale-down object-center rounded self-center">
+            <OnImagesLoaded onLoaded={()=> setImgLoaded(true)} >        
+            <div className={imgLoaded ? 'hidden-false' : 'hidden-true'}>
+                <img alt={props.title} className="image" src={props.imgUrl}/>
+            </div>
+              {  !imgLoaded && <ImageLoader /> }      
+            </OnImagesLoaded>
           </div>
           <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mb-6 lg:mb-0">
             <h2 className="text-sm uppercase title-font text-gray-500 tracking-widest">
@@ -56,29 +64,23 @@ export default function ItemDetail(props) {
             <div className="flex">
               <span className="title-font font-bold text-2xl text-indigo-600">
                 ${props.price}
-              </span>              
+              </span>
             </div>
-            {!isInCart(props.id) ?
-              <ItemCount 
-                stock={props.stock} 
-                initial={1} 
-                onAdd={onAdd}
-              />
-            : 
-            <div>
-              <button 
-                  className="flex mx-auto mt-2 text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">
+            {!isInCart(props.id) ? (
+              <ItemCount stock={props.stock} initial={1} onAdd={onAdd} />
+            ) : (
+              <div>
+                <button className="flex mx-auto mt-2 text-white bg-green-500 border-0 py-2 px-8 focus:outline-none hover:bg-green-600 rounded text-lg">
                   <Link to="/cart">Finalizar compra</Link>
-              </button>
-              <div className="text-center mt-2 leading-none flex-wrap justify-centerw-full py-2">                      
-              <span className="w-full block py-1 px-2 rounded bg-green-50 text-green-500 text-xs font-medium tracking-widest">
+                </button>
+                <div className="text-center mt-2 leading-none flex-wrap justify-centerw-full py-2">
+                  <span className="w-full block py-1 px-2 rounded bg-green-50 text-green-500 text-xs font-medium tracking-widest">
                     Ya cargaste {qtyInCart(props.id)} en el carrito
-              </span>                 
+                  </span>
+                </div>
               </div>
-            </div>            
-            }
+            )}
           </div>
-         
         </div>
         <button
           onClick={() => navig.go(-1)}
@@ -88,16 +90,20 @@ export default function ItemDetail(props) {
         </button>
       </div>
 
-      <div className="absolute left-0 top-1/2">      
-           <button onClick={()=>props.goNext(-1)} disabled={props.index === 1} className="w-2/4 text-grey-400">
-            <FontAwesomeIcon icon={faArrowCircleLeft} />
-          </button>
-      </div>     
-        <div className="absolute right-0 top-1/2">
-          <button onClick={()=>props.goNext(1)} className="w-2/4 text-grey-400">
-              <FontAwesomeIcon icon={faArrowCircleRight} />
-          </button>
-        </div>
+      <div className="absolute left-0 top-1/2">
+        <button
+          onClick={() => props.goNext(-1)}
+          disabled={props.index === 1}
+          className="w-2/4 text-grey-400"
+        >
+          <FontAwesomeIcon icon={faArrowCircleLeft} />
+        </button>
+      </div>
+      <div className="absolute right-0 top-1/2">
+        <button onClick={() => props.goNext(1)} className="w-2/4 text-grey-400">
+          <FontAwesomeIcon icon={faArrowCircleRight} />
+        </button>
+      </div>
     </section>
   );
 }
---
+++ src/components/loaders/ImgLoader.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const ImgLoader = () => {
+  return (
+        <div className="mx-auto animate-pulse flex space-x-12">
+                <div className="m-auto rounded-full bg-gray-300 h-96 w-96"></div>           
+            </div>
+  );
+};
+
+export default ImgLoader;
+
